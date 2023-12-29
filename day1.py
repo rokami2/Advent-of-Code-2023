4c9eb4f017b9a17e3831c19708d4eeb4cdf78ce1
Emma Wiechert <emmawiechert212@gmail.com>
2023-12-29 15:17:50 -0500

    updated the logic behind too many numbers being pulled to be more accurate

--- day1.py
+++ day1.py
@@ -1,11 +1,10 @@
 #imports
 import re
-import numpy as np
 
 #initalize vars
 numarray = []
 index = 0
-codeindex = -1
+norepeat = 0
 num = ""
 converted_int = 0
 code = 0
@@ -16,7 +15,6 @@ total = 0
 with open("input.txt", "r") as cypher:
         
         linenum = re.findall("[0-9]+", cypher.read())
-        print(linenum)
 
         if linenum:
             numarray += linenum
@@ -26,13 +24,13 @@ with open("input.txt", "r") as cypher:
 num = numarray[index]     
 
 for index in range(0, len(numarray), 2):
-
+    print("DEBUG: ", index)
     # check if the number isn't two digits and change accordingly for first number
     if int(num) < 10:
         converted_int = num + num
     # logic to check if the number is too high and change the number accordingly
     elif int(num) > 99:
-        converted_int = num[0:2]
+        converted_int = num[0] + num[-1]
     else:
         converted_int = num
 
@@ -43,17 +41,20 @@ for index in range(0, len(numarray), 2):
         converted_int2 = num + num
     # check if the number is too high and change the number accordingly
     elif int(num) > 99:
-        converted_int2 = num[0:2]
+        converted_int2 = num[0] + num[-1]
     else:
         converted_int2 = num
 
-    if index % 2 == 0:
+
+    norepeat += 1
+    if norepeat % 2 == 0:
+        print("DEBUG: Add to results")
         total = int(converted_int) + int(converted_int2)
         print(total, "=", converted_int, "+", converted_int2)
         code += total
-        converted_int = converted_int2
+        
     
-    index += 1
+    
 
 #prints the result out for input on advent of code website
 print (code)
